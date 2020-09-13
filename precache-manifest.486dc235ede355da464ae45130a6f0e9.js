68e10b6a3b39e661e5ad8ac6db80b103d8bb5127
Grzywa1234 <47593899+Grzywa1234@users.noreply.github.com>
2020-09-13 09:18:56 +0200

    aviationinfo production

---
+++ precache-manifest.486dc235ede355da464ae45130a6f0e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29eaace60fc56130b52ac6c2f62a32e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76048cf435dd074acc5e",
+    "url": "/static/css/main.25a9f411.chunk.css"
+  },
+  {
+    "revision": "62ce167a1ae05011d50f",
+    "url": "/static/js/2.0f306e36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f306e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76048cf435dd074acc5e",
+    "url": "/static/js/main.3c3bafc0.chunk.js"
+  },
+  {
+    "revision": "3e592ef704de36790278",
+    "url": "/static/js/runtime-main.033c7639.js"
+  },
+  {
+    "revision": "7b6e423bb64a321a210920b5845b61f2",
+    "url": "/static/media/chris-leipelt-4UgUpo3YdKk-unsplash.7b6e423b.jpg"
+  },
+  {
+    "revision": "da71fa7248b85547dcf9d3714d9f8f28",
+    "url": "/static/media/shaun-darwood-TC6u_HnDDqs-unsplash.da71fa72.jpg"
+  }
+]);
